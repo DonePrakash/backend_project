7932d812edf931959eb8de14f7b6e8365771948b
Prakash <krprakash9128@gmail.com>
2025-02-23 19:32:45 +0530

    add register controller and bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,95 @@
 import  { asyncHandler }  from "../utils/asyncHandler.js";
+import { ApiError } from  "../utils/apiError.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/apiResponse.js";
 
 
 const registerUser = asyncHandler( async (req,res) => {
-    res.status(200).json({
-        message:"OK"
+    // get user details from frontend
+    // validation - not empty
+    // check if user already exists : username,email
+    // check for images, check for avatar
+    // upload them to cloudinary but must check avatar
+    // create user object - create entry in db
+    // remove password and refresh token field from response
+    // check for user creation
+    // return response
+
+    // get user details from frontend - here we are doing it using postman
+    const {fullName,email,username,password} = req.body
+    console.log("email: ",email);
+    
+    // if(fullName === ""){
+    //     throw new ApiError(400, "fullname is required")
+    // }
+
+
+    // validation - not empty
+    if(
+        [fullName, email, username, password].some((field) =>
+        field?.trim() === "")
+    ){
+        throw new ApiError(400, "All fields are required")
+    }
+
+    // check if user already exists : username,email
+    const existedUser = User.findOne({
+        $or:[{ username }, { email }]
     })
-})
+
+    if(existedUser){
+        throw new ApiError(409, "User with email and username already exists")
+    }
+
+    // check for images, check for avatar
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    if(!avatarLocalPath){
+        throw new ApiError(400, "Avatar file is required");
+    }
+
+    // upload them to cloudinary but must check avatar
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+    if(!avatar){
+        throw new ApiError(400, "fullname is required")
+    }
+
+    // create user object - create entry in db
+    const user = await User.create({
+        fullName,
+        avatar:avatar.url,
+        coverImage: coverImage?.url || "",
+        email,
+        password,
+        username: username.toLowerCase()
+    })
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    // check for user creation
+    if(!createdUser){
+        throw new ApiError(500,"Something went wrong while registering the user")
+    }
+
+    // return response
+    return res.status(201).json(
+        new ApiResponse(200, createdUser, "User registered Successfully")
+    )
+    
+})  
+
+
+
 
 export {registerUser}
+
+/*
+    1. This line imports the asyncHandler utility from the utils folder.
+    2. asyncHandler is typically a middleware function that wraps asynchronous route handlers to 
+        manage errors gracefully, avoiding the need for multiple try-catch blocks.
+*/
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,32 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router()
 
-router.route("/register").post(registerUser)
+router.route("/register").post(
+    upload.fields([
+        {
+            name : "avatar",
+            maxCount:1
+        },
+        {
+            name:"coverImage",
+            maxCount:1
+        }
+    ]),
+    registerUser
+)
 
 export default router
+
+/*
+    When a POST request is made to this URL:
+    1. It goes through the userRouter.
+    2. Matches the /register route.
+    3. Executes the registerUser function.
+    4. Responds with:
+        {
+            "message": "OK"
+        }
+*/
